4f387f90dcfabafb0b887047c777a619ed833a0e
Martin Munilla <martinmunillas@gmail.com>
2023-12-02 21:38:05 +0100

    feat: first problem

--- first_a/src/main.rs
+++ first_a/src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("input.txt");
+    let lines = input.lines();
+    let mut result = 0;
+    for l in lines {
+        result += line_value(l);
+    }
+    println!("Result: {}", result);
+}
+
+fn line_value(x: &str) -> i32 {
+    let mut value = 0;
+    for c in x.chars() {
+        match c.to_digit(10) {
+            Some(n) => {
+                value += (n as i32) * 10;
+                break;
+            }
+            None => continue,
+        }
+    }
+    for c in x.chars().rev() {
+        match c.to_digit(10) {
+            Some(n) => {
+                value += n as i32;
+                break;
+            }
+            None => continue,
+        }
+    }
+    value
 }
