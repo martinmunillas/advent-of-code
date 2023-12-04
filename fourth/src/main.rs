e485a90f9ec67604c83e071e5139698d65973044
Martin Munilla <martinmunillas@gmail.com>
2023-12-04 08:38:04 +0100

    feat: fourth day b

--- fourth/src/main.rs
+++ fourth/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Card {
     ideal: Vec<i32>,
     actual: Vec<i32>,
@@ -11,11 +12,34 @@ fn main() {
         .iter()
         .map(line_to_card)
         .collect();
+    println!("Result A: {}", total_card_pts(&cards));
+    println!("Result B: {}", total_accumulative_cards(&cards));
+}
+
+fn total_accumulative_cards(cards: &[Card]) -> i32 {
+    let mut accumulated = 0;
+    let card_matches = cards
+        .iter()
+        .map(card_matching_numbers)
+        .collect::<Vec<i32>>();
+    let mut times: Vec<i32> = cards.iter().map(|_| 1).collect();
+
+    for (i, matching) in card_matches.iter().enumerate() {
+        for j in i + 1..=i + *matching as usize {
+            times[j] += times[i];
+        }
+        accumulated += times[i]
+    }
+
+    accumulated
+}
+
+fn total_card_pts(cards: &[Card]) -> i32 {
     let mut pts = 0;
     for card in cards.iter() {
         pts += card_pts(card);
     }
-    println!("Result: {}", pts);
+    pts
 }
 
 fn serialize_vec(string: &str) -> Vec<i32> {
@@ -35,13 +59,23 @@ fn line_to_card(line: &&str) -> Card {
 
 fn card_pts(card: &Card) -> i32 {
     let mut pts = 0;
-    for x in card.actual.iter() {
+    for x in card.actual.clone().iter() {
         if card.ideal.contains(x) {
             if pts == 0 {
                 pts = 1;
             } else {
                 pts *= 2;
             }
+        }
+    }
+    pts
+}
+
+fn card_matching_numbers(card: &Card) -> i32 {
+    let mut pts = 0;
+    for x in card.actual.clone().iter() {
+        if card.ideal.contains(x) {
+            pts += 1;
         }
     }
     pts
