aee743b700a3bf51bef0e400079d124c98e571e0
Martin Munilla <martinmunillas@gmail.com>
2023-12-09 07:28:30 +0100

    chore: cleanup 2022 day 1

--- 2022/1/index.test.ts
+++ 2022/1/index.test.ts
@@ -1,46 +1,26 @@
-import { getMostCalories } from ".";
+import { readFileSync } from "fs";
+import { getMostCalories, getTop3CaloriesAddedUp } from ".";
+import { resolve } from "path";
 
-describe("First challenge", () => {
+const test1 = readFileSync(resolve(__dirname, "./input/example1.txt"), "utf-8");
+const test2 = readFileSync(resolve(__dirname, "./input/example2.txt"), "utf-8");
+
+describe("First day part a examples", () => {
   it("should retrieve the 24000 calories", () => {
-    expect(
-      getMostCalories(
-        `1000
-2000
-3000
-
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000`
-      )
-    ).toBe(24000);
+    expect(getMostCalories(test1)).toBe(24000);
   });
 
   it("should retrieve the 161 calories", () => {
-    expect(
-      getMostCalories(
-        `23
-4
+    expect(getMostCalories(test2)).toBe(161);
+  });
+});
 
-17
-40
+describe("First day part b examples", () => {
+  it("should retrieve the 24000 calories", () => {
+    expect(getTop3CaloriesAddedUp(test1)).toBe(45000);
+  });
 
-50
-6
-
-70
-82
-9
-
-12
-20`
-      )
-    ).toBe(161);
+  it("should retrieve the 161 calories", () => {
+    expect(getTop3CaloriesAddedUp(test2)).toBe(274);
   });
 });
