3eda884f30d6874e23741d807235d389cd53979e
Martin Munilla <martinmunillas@gmail.com>
2023-12-11 08:12:55 +0100

    feat: day 11 part b

--- 2023/eleventh/src/main.rs
+++ 2023/eleventh/src/main.rs
@@ -6,10 +6,11 @@ fn main() {
     let input = include_str!("./input/input.txt");
     let universe = collect_universe(input);
 
-    println!("Result 1: {}", sum_galaxy_distances(universe));
+    println!("Result 1: {}", sum_galaxy_distances(&universe, 2));
+    println!("Result 2: {}", sum_galaxy_distances(&universe, 1_000_000));
 }
 
-fn sum_galaxy_distances(universe: Vec<Vec<char>>) -> i32 {
+fn sum_galaxy_distances(universe: &Vec<Vec<char>>, expansion_rate: i64) -> i64 {
     let mut has_galaxy: HashMap<(char, usize), bool> = HashMap::new();
     let mut galaxies: Vec<(usize, usize)> = Vec::new();
 
@@ -30,7 +31,7 @@ fn sum_galaxy_distances(universe: Vec<Vec<char>>) -> i32 {
         y_offsets.push(current_offset);
 
         if has_galaxy.get(&('y', y)).is_none() {
-            current_offset += 1;
+            current_offset += expansion_rate - 1;
         }
     }
 
@@ -39,7 +40,7 @@ fn sum_galaxy_distances(universe: Vec<Vec<char>>) -> i32 {
         x_offsets.push(current_offset);
 
         if has_galaxy.get(&('x', x)).is_none() {
-            current_offset += 1;
+            current_offset += expansion_rate - 1;
         }
     }
 
@@ -49,11 +50,10 @@ fn sum_galaxy_distances(universe: Vec<Vec<char>>) -> i32 {
         for j in (i + 1)..galaxies.len() {
             let b = &galaxies[j];
 
-            let real_a = (a.0 + x_offsets[a.0], a.1 + y_offsets[a.1]);
-            let real_b = (b.0 + x_offsets[b.0], b.1 + y_offsets[b.1]);
+            let real_a = (a.0 as i64 + x_offsets[a.0], a.1 as i64 + y_offsets[a.1]);
+            let real_b = (b.0 as i64 + x_offsets[b.0], b.1 as i64 + y_offsets[b.1]);
 
-            let distance = (real_a.0 as i32 - real_b.0 as i32).abs()
-                + (real_a.1 as i32 - real_b.1 as i32).abs();
+            let distance = (real_a.0 - real_b.0).abs() + (real_a.1 - real_b.1).abs();
 
             sum += distance;
         }
@@ -69,6 +69,17 @@ fn collect_universe(input: &str) -> Vec<Vec<char>> {
 fn run_tests() {
     let example = include_str!("./input/example.txt");
 
-    assert_eq!(sum_galaxy_distances(collect_universe(example)), 374);
-    println!("Tests passed!")
+    let universe = collect_universe(example);
+    assert_eq!(sum_galaxy_distances(&universe, 2), 374);
+    println!("Test with expansion rate 2 passed!");
+
+    assert_eq!(sum_galaxy_distances(&universe, 10), 1030);
+    println!("Test with expansion rate 10 passed!");
+
+    assert_eq!(sum_galaxy_distances(&universe, 100), 8410);
+    println!("Test with expansion rate 100 passed!");
+
+    println!("");
+    println!("Tests passed!");
+    println!("");
 }
