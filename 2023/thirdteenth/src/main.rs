3d088c34db232ff645d65c33433fb1bb38ace420
Martin Munilla <martinmunillas@gmail.com>
2023-12-13 10:52:55 +0100

    feat: day 13 part b

--- 2023/thirdteenth/src/main.rs
+++ 2023/thirdteenth/src/main.rs
@@ -3,16 +3,16 @@ fn main() {
     run_tests();
 
     let input = include_str!("./input/input.txt");
-    println!("Result A: {}", sum_mirrors(&collect_mazes(input)));
-    println!("Result B: ");
+    println!("Result A: {}", sum_mirrors(&collect_mazes(input), 0));
+    println!("Result B: {}", sum_mirrors(&collect_mazes(input), 1));
 }
 
-fn sum_mirrors(mazes: &Vec<Vec<Vec<char>>>) -> i32 {
+fn sum_mirrors(mazes: &Vec<Vec<Vec<char>>>, expected_differences: i32) -> i32 {
     let mut sum = 0;
     for maze in mazes {
-        let xresult = find_x_mirror(maze);
+        let xresult = find_x_mirror(maze, expected_differences);
         sum += xresult;
-        let yresult = find_y_mirror(maze);
+        let yresult = find_y_mirror(maze, expected_differences);
         sum += yresult * 100;
     }
     sum
@@ -25,62 +25,44 @@ fn collect_mazes(input: &str) -> Vec<Vec<Vec<char>>> {
         .collect()
 }
 
-fn find_x_mirror(maze: &Vec<Vec<char>>) -> i32 {
-    let mut possible_mirrorings = Vec::new();
+fn find_x_mirror(maze: &Vec<Vec<char>>, expected_differences: i32) -> i32 {
     let line_lenght = maze[0].len();
-    'searching: for i in 1..line_lenght {
-        'limits: for j in 1..line_lenght {
-            if i < j || i + j - 1 >= line_lenght {
-                break 'limits;
-            }
-            if maze[0][i - j] != maze[0][i + j - 1] {
-                continue 'searching;
-            }
-        }
-        possible_mirrorings.push(i);
-    }
 
-    'searching: for possible in possible_mirrorings {
+    for possible in 1..line_lenght {
+        let mut differences = 0;
         for y in 0..maze.len() {
             let search_length = min(line_lenght - possible + 1, possible + 1);
             for x in 1..search_length {
                 if maze[y][possible - x] != maze[y][possible + x - 1] {
-                    continue 'searching;
+                    differences += 1;
                 }
             }
         }
-        return possible as i32;
+        if differences == expected_differences {
+            return possible as i32;
+        }
     }
 
     0
 }
 
-fn find_y_mirror(maze: &Vec<Vec<char>>) -> i32 {
-    let mut possible_mirrorings = Vec::new();
+fn find_y_mirror(maze: &Vec<Vec<char>>, expected_differences: i32) -> i32 {
     let line_lenght = maze[0].len();
     let lines = maze.len();
-    'searching: for i in 1..lines {
-        'limits: for j in 1..lines {
-            if i < j || i + j - 1 >= lines {
-                break 'limits;
-            }
-            if maze[i - j][0] != maze[i + j - 1][0] {
-                continue 'searching;
-            }
-        }
-        possible_mirrorings.push(i);
-    }
 
-    'searching: for possible in possible_mirrorings {
+    for possible in 1..lines {
+        let mut differences = 0;
         for x in 0..line_lenght {
             let search_length = min(lines - possible + 1, possible + 1);
             for y in 1..search_length {
                 if maze[possible - y][x] != maze[possible + y - 1][x] {
-                    continue 'searching;
+                    differences += 1;
                 }
             }
         }
-        return possible as i32;
+        if differences == expected_differences {
+            return possible as i32;
+        }
     }
 
     0
@@ -89,7 +71,9 @@ fn find_y_mirror(maze: &Vec<Vec<char>>) -> i32 {
 fn run_tests() {
     let example = include_str!("./input/example.txt");
 
-    assert_eq!(sum_mirrors(&collect_mazes(example)), 405);
+    assert_eq!(sum_mirrors(&collect_mazes(example), 0), 405);
+    println!("Test passed!");
+    assert_eq!(sum_mirrors(&collect_mazes(example), 1), 400);
     println!("Test passed!");
 
     println!("");
