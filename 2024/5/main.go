30bfacf06e4e2f1e34198b9c311041b5eddec3b8
Martin Munilla <martinmunillas@gmail.com>
2024-12-05 06:35:23 +0100

    feat: day 5 part a

---
+++ 2024/5/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"os"
+	"slices"
+	"strconv"
+)
+
+func parseOrderings(file string) (map[int][]int, [][]int) {
+	f, err := os.ReadFile(file)
+	if err != nil {
+		panic(err)
+	}
+	orderings := map[int][]int{}
+	updates := [][]int{}
+	currNumber := ""
+	currLine := []int{}
+	isUpdates := false
+	for i, c := range f {
+		if c == '\n' && f[i-1] == '\n' {
+			isUpdates = true
+			continue
+		} else if c == '|' || c == '\n' || c == ',' {
+			n, err := strconv.Atoi(currNumber)
+			if err != nil {
+				panic(err)
+			}
+			currLine = append(currLine, n)
+			currNumber = ""
+		} else {
+			currNumber += string(c)
+		}
+
+		if c == '\n' {
+			if isUpdates {
+				updates = append(updates, currLine)
+			} else {
+				if _, ok := orderings[currLine[0]]; !ok {
+					orderings[currLine[0]] = []int{currLine[1]}
+				} else {
+					orderings[currLine[0]] = append(orderings[currLine[0]], currLine[1])
+				}
+			}
+			currLine = nil
+		}
+	}
+	updates = append(updates, currLine)
+	return orderings, updates
+}
+
+func A(file string) int {
+	result := 0
+	orderings, updates := parseOrderings(file)
+looking:
+	for _, update := range updates {
+		for i, n := range update[:len(update)-1] {
+			m := update[i+1]
+			if !slices.Contains(orderings[n], m) {
+				continue looking
+			}
+		}
+		result += update[int(float32(len(update)-1)/2+0.6)]
+	}
+
+	return result
+}
+
+func B(file string) int {
+	result := 0
+
+	return result
+}
