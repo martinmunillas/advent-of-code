5a59198e604a22709a7110d3fc908452a446d046
Martin Munilla <martinmunillas@gmail.com>
2024-12-26 20:06:03 -0300

    feat: more catch up

--- 2024/20/main.go
+++ 2024/20/main.go
@@ -1,9 +1,186 @@
 package main
 
+import (
+	"fmt"
+	"image"
+	"math"
+	"os"
+	"strings"
+)
+
+type Image [][]rune
+
+func (i Image) In(p image.Point) rune {
+	return i[p.Y][p.X]
+}
+
+func (i Image) Set(p image.Point, r rune) {
+	i[p.Y][p.X] = r
+}
+func (i Image) Copy() Image {
+	newImage := make(Image, len(i))
+	for j := range newImage {
+		newLine := make([]rune, len(i[j]))
+		copy(newLine, i[j])
+		newImage[j] = newLine
+	}
+
+	return newImage
+}
+
+func (i Image) InBounds(p image.Point) bool {
+	return p.X >= 0 && p.Y >= 0 && p.X < len(i[0]) && p.Y < len(i)
+}
+
+func (m Image) Print() {
+	for _, l := range m {
+		fmt.Println(string(l))
+	}
+}
+
+type Distances [][]int
+
+func (i Distances) In(p image.Point) int {
+	return i[p.Y][p.X]
+}
+
+func (i Distances) Set(p image.Point, r int) {
+	i[p.Y][p.X] = r
+}
+
+func (m Distances) Print() {
+	for _, l := range m {
+		for _, n := range l {
+			fmt.Printf("|%d|", n)
+		}
+		fmt.Println()
+	}
+}
+
+func parse(file string) (Image, image.Point) {
+	f, err := os.ReadFile(file)
+	if err != nil {
+		panic(err)
+	}
+	img := Image{}
+	var start image.Point
+	for i, line := range strings.Split(string(f), "\n") {
+		img = append(img, []rune(line))
+		xStart := strings.Index(line, "S")
+		if xStart != -1 {
+			start = image.Pt(xStart, i)
+		}
+	}
+
+	img.Set(start, '.')
+
+	return img, start
+}
+
+func getDistances(img Image, coord image.Point) Distances {
+	distances := make(Distances, len(img))
+	for i := range distances {
+		distances[i] = make([]int, len(img[i]))
+	}
+	for i, line := range img {
+		for j, c := range line {
+			if c == '#' {
+				distances[i][j] = -1
+			}
+		}
+	}
+	distance := 0
+traveling:
+	for {
+		distances.Set(coord, distance)
+		img.Set(coord, '#')
+		for _, dir := range []image.Point{
+			{1, 0},
+			{-1, 0},
+			{0, -1},
+			{0, 1},
+		} {
+			newCoord := coord.Add(dir)
+			if img.In(newCoord) == '.' || img.In(newCoord) == 'E' {
+				coord = newCoord
+				distance++
+				continue traveling
+			}
+		}
+		break
+	}
+
+	return distances
+
+}
+
 func A(file string) int {
-	return 0
+	img, start := parse(file)
+
+	distances := getDistances(img.Copy(), start)
+
+	result := 0
+
+	for i, line := range img {
+		for j, c := range line {
+			if c == '#' {
+				continue
+			}
+
+			pos := image.Pt(j, i)
+			for _, possibleCheat := range []image.Point{
+				{2, 0},
+				{1, 1},
+				{0, 2},
+				{-1, 1},
+			} {
+				cheatPos := pos.Add(possibleCheat)
+				if img.InBounds(cheatPos) && img.In(cheatPos) != '#' && math.Abs(float64(distances.In(cheatPos)-distances.In(pos))) > 101 {
+					result++
+				}
+			}
+		}
+	}
+
+	return result
 }
 
 func B(file string) int {
-	return 0
+	img, start := parse(file)
+
+	distances := getDistances(img.Copy(), start)
+
+	result := 0
+
+	for i, line := range img {
+		for j, c := range line {
+			if c == '#' {
+				continue
+			}
+
+			pos := image.Pt(j, i)
+			for r := range 21 {
+				if r < 2 {
+					continue
+				}
+				for di := range r + 1 {
+					dj := r - di
+
+					for possibleCheat := range map[image.Point]interface{}{
+						image.Pt(dj, di):   true,
+						image.Pt(dj, -di):  true,
+						image.Pt(-dj, -di): true,
+						image.Pt(-dj, di):  true,
+					} {
+						cheatPos := pos.Add(possibleCheat)
+						if img.InBounds(cheatPos) && img.In(cheatPos) != '#' && distances.In(pos)-distances.In(cheatPos) >= 100+r {
+							result++
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return result
 }
--- 2024/21/main.go
+++ 2024/21/main.go
@@ -1,7 +1,102 @@
 package main
 
+import (
+	"fmt"
+	"image"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func parse(file string) []string {
+	f, err := os.ReadFile(file)
+	if err != nil {
+		panic(err)
+	}
+	return strings.Split(string(f), "\n")
+}
+
+var numericCoords = map[rune]image.Point{
+	'7': image.Pt(0, 0),
+	'8': image.Pt(1, 0),
+	'9': image.Pt(2, 0),
+	'4': image.Pt(0, 1),
+	'5': image.Pt(1, 1),
+	'6': image.Pt(2, 1),
+	'1': image.Pt(0, 2),
+	'2': image.Pt(1, 2),
+	'3': image.Pt(2, 2),
+	'0': image.Pt(1, 3),
+	'A': image.Pt(2, 3),
+}
+
+var directionalCoords = map[rune]image.Point{
+	'^': image.Pt(1, 0),
+	'A': image.Pt(2, 0),
+	'<': image.Pt(0, 1),
+	'v': image.Pt(1, 1),
+	'>': image.Pt(2, 1),
+}
+
+func fromTo(coords map[rune]image.Point, origin rune, target rune) []rune {
+	diff := coords[target].Sub(coords[origin])
+	xNegative := diff.X < 0
+	yNegative := diff.Y < 0
+	if xNegative {
+		diff.X += -(diff.X * 2)
+	}
+	if yNegative {
+		diff.Y += -(diff.Y * 2)
+	}
+	result := []rune{}
+	for range diff.Y {
+		if yNegative {
+			result = append(result, '^')
+		} else {
+			result = append(result, 'v')
+		}
+	}
+
+	for range diff.X {
+		if xNegative {
+			result = append(result, '<')
+		} else {
+			result = append(result, '>')
+		}
+	}
+
+	result = append(result, 'A')
+	return result
+}
+
+func compute(coords map[rune]image.Point) func([]rune) []rune {
+	return func(code []rune) (all []rune) {
+		prev := 'A'
+		for _, curr := range code {
+			all = append(all, fromTo(coords, prev, curr)...)
+			prev = curr
+		}
+		return all
+	}
+}
+
+var directional = compute(directionalCoords)
+var numeric = compute(numericCoords)
+
 func A(file string) int {
-	return 0
+	codes := parse(file)
+	result := 0
+	for _, code := range codes {
+		numPart, err := strconv.Atoi(code[:3])
+		if err != nil {
+			panic(err)
+		}
+		instructions := directional(directional(numeric([]rune(code))))
+		fmt.Println(string(instructions))
+		fmt.Println(numPart, len(instructions))
+		result += numPart * len(instructions)
+	}
+	return result
 }
 
 func B(file string) int {
